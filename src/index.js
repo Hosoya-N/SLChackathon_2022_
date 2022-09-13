fd10f40a7f6219d27112fd457c91a22262252398
長谷部真理 <mariuni1109@gmail.com>
2022-09-13 07:15:48 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
 
-`;
+let counter, btnAdd;
+let n;
+ 
+function addCount(){
+    n++;
+    counter.innerHTML = n;
+}
+ 
+window.addEventListener("load", ()=>{
+    // 起動時の処理
+    counter = document.getElementById("counter");
+    btnAdd = document.getElementById("btnAdd");
+    n = counter.innerHTML;
+ 
+    btnAdd.addEventListener("click", addCount);
+});
+
